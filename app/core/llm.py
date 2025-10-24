a1f01bdc03e80bf66833bb162e336184fc35368e
Erasil <erasil.bakhytgan@gmail.com>
2025-10-24 10:53:27 +0500

    change to model open ai

--- app/core/llm.py
+++ app/core/llm.py
@@ -1,10 +1,21 @@
-from langchain_groq import ChatGroq
+# from langchain_groq import ChatGroq
+# from .config import settings
+#
+# # создаётся один раз на импорт
+# llm = ChatGroq(
+#     model="openai/gpt-oss-120b",
+#     groq_api_key=settings.GROQ_API_KEY,
+#     temperature=0.0,
+#     max_tokens=2048,
+# )
+
+from langchain_openai import ChatOpenAI
 from .config import settings
 
-# создаётся один раз на импорт
-llm = ChatGroq(
-    model="openai/gpt-oss-120b",
-    groq_api_key=settings.GROQ_API_KEY,
+# Создание клиента для OpenAI
+llm = ChatOpenAI(
+    model="gpt-3.5-turbo",
+    openai_api_key=settings.OPENAI_SECRET_KEY,
     temperature=0.0,
     max_tokens=2048,
 )
