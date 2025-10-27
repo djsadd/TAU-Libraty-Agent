234ed0d09b6b020f9b04fb1422f8dde8a56fa333
Erasil <erasil.bakhytgan@gmail.com>
2025-10-27 16:25:20 +0500

    TEST

--- book-chat/src/components/ChatBox.tsx
+++ book-chat/src/components/ChatBox.tsx
@@ -125,7 +125,7 @@ const BookModal: React.FC<{
                 <span>Печатаю…</span>
               </div>
             )}
-
+            <p>Читать онлайн</p>
             <div className="p-3 bg-gray-50 border border-tau-primary/10 rounded-lg text-sm text-gray-700 min-h-[120px]">
               {typeof streamed === "string" ? (
                 <div
