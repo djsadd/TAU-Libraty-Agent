0eb431e6365e4c9bc8857a504fb57d56e3d8bfab
Erasil <erasil.bakhytgan@gmail.com>
2025-10-28 11:36:43 +0500

    TE

--- book-chat/src/App.tsx
+++ book-chat/src/App.tsx
@@ -13,7 +13,7 @@ export default function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/app" element={<ChatBox />} />
-        <Route path="*" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
